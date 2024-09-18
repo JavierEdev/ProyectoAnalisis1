c63ff2637528501219069444a653f1af392bb839
nicki0066 <nicoleiboy98@gmail.com>
2024-09-17 18:20:10 -0600

    commit: Update Administrador
    Cambios de direccionamiento, agregar el JS para carga de espacios segun el Id_Condo

---
+++ Frontend/js/espacio.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const idUsuario = localStorage.getItem('idUsuario');
+    const idCondo = localStorage.getItem('idCondo');
+    const userName = localStorage.getItem('userName');
+    const id_Rol = localStorage.getItem('id_Rol');
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        console.log("Id_Rol:", id_Rol); 
+        console.log("Id_Condo:", idCondo); 
+        document.getElementById('username').textContent = userName;
+    } else {
+        console.log("No se encontró el nombre de usuario en localStorage");
+    }
+
+    fetch('http://localhost/ProyectoAnalisis1/Backend/index.php/espacios', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id_condo: idCondo })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error en la solicitud");
+        }
+        return response.json();
+    })
+    .then(result => {
+        console.log(result); 
+        populateTable(result); 
+    })
+    .catch(error => {
+        console.error("Error al obtener los espacios:", error);
+        alert('Error al obtener los espacios.');
+    });
+});
+
+// Llenar tabla del espaciosAdmin.html
+function populateTable(data) {
+    const tableBody = document.querySelector('#example tbody');
+    tableBody.innerHTML = ''; 
+    
+
+    // espacio.id_espacio
+    data.forEach(espacio => {
+        const row = `
+            <tr>
+                <td>${espacio.nombre}</td>
+                <td>${espacio.descripcion}</td>
+                <td>${espacio.ubicacion}</td>
+                <td>${espacio.mantenimiento}</td>
+                <td>${espacio.estado}</td>
+                <td>${espacio.condominio}</td>
+                <td>
+                    <a href="#" class="btn btn-primary btn-sm">Ver</a>
+                    <a href="#" class="btn btn-primary btn-sm">Editar</a>
+                </td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+}
